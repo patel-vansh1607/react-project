641a00f11a6bada68a28f29a6112f50ca3f97936
Vansh Patel <patel.vansh1607@gmail.com>
2025-02-20 17:04:10 +0300

    add/remove files

--- src/components/counter.component.js
+++
@@ -1,4 +0,0 @@
-function counterComponent(){
-
-}
-export default counterComponent
---
+++ src/components/headerComponent.js
@@ -0,0 +1,15 @@
+function counterComponent(){
+    return(
+        <div>
+            <HeaderComponent/>
+        </div>
+    )
+}
+
+
+function HeaderComponent(){
+    return (
+        <h1>My First React Project </h1>
+    )
+}
+export default counterComponent
